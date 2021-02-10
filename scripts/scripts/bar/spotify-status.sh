f33c5a157df93a106f9873ace6224fd6622d495c
Luis Simas <luissimas@protonmail.com>
2021-02-10 14:44:00 +0000

    fixing script error

--- scripts/scripts/bar/spotify-status.sh
+++ scripts/scripts/bar/spotify-status.sh
@@ -7,7 +7,7 @@ formatString="{{ title }} - {{ artist }}"
 playerctlStatus=$(playerctl --player=$player status 2>/dev/null)
 
 # Only show data if there is a music playing at the moment
-if [ $playerctlStatus = "Playing" ]; then
+if [ "$playerctlStatus" = "Playing" ]; then
   musicData="$(playerctl --player=$player metadata --format "$formatString")"
   echo "  $musicData"
 else
