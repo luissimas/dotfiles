f1b2298e8e1fad1784eadb99cf14aa0294b8f85a
Luis Simas <luissimas@protonmail.com>
2021-02-14 20:54:33 +0000

    adding youtube web scraper script

--- scripts/scripts/ytb.sh
+++ scripts/scripts/ytb.sh
@@ -2,9 +2,6 @@
 
 # This script was copied from https://github.com/Bugswriter/myyt and was modified for my own needs
 
-# Your api key (its better to make that a cat command)
-YT_API_KEY=$(cat ~/.ytbapikey.txt)
-
 # If no arguments are given, a rofi prompt is shown
 if [[ -z "$1" ]]; then
   query=$(rofi -dmenu -p "Search " -l 0)
@@ -17,11 +14,8 @@ if [[ -n "$query" && "$query" != " " ]]; then
   # Changing spaces to '+'
   query="${query// /+}"
 
-  # Url string for the api
-  urlstring="https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&maxResults=10&key=${YT_API_KEY}"
-
-  # Get search results and pipe into rofi
-  selectedUrl="https://$(curl -s $urlstring | jq -r '.items[] | "\(.snippet.channelTitle) => \(.snippet.title) => youtu.be/\(.id.videoId)"' | rofi -dmenu -i -p "Select a video " -l 10 -width 80 | awk '{print $NF}')"
+  # Get search results and pipe into rofi using python web scraper
+  selectedUrl=$(python ~/scripts/youtube-scraper.py $query | rofi -dmenu -i -p "Select a video " -l 10 -width 80 | awk '{print $NF}')
 
   # Checks if any video was selected
   if [[ -n "$selectedUrl" && "$selectedUrl" != "https://" ]]; then
