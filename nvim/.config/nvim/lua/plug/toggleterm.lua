576aca290dae1ccc8053281adc37805480dce80e
Luis Simas <luissimas@protonmail.com>
2021-04-25 16:15:32 -0300

    New configs on toggleterm

--- nvim/.config/nvim/lua/plug/toggleterm.lua
+++ nvim/.config/nvim/lua/plug/toggleterm.lua
@@ -2,11 +2,12 @@ require"toggleterm".setup{
   size = 10,
   --open_mapping = [[<leader>t]],
   open_mapping = [[<C-t>]],
+  hide_numbers = true,
   insert_mappings = false,
   shade_filetypes = {},
   shade_terminals = false,
   shading_factor = '<number>', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
   start_in_insert = true,
-  persist_size = true,
+  persist_size = false,
   direction = 'horizontal'
 }
