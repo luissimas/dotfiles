5534e9d478e797e8cd67841d5dc16706b3269653
Luis Simas <luissimas@protonmail.com>
2021-06-04 09:58:53 -0300

    Fixing error display on packages script

--- scripts/scripts/bar/packages.sh
+++ scripts/scripts/bar/packages.sh
@@ -3,7 +3,7 @@
 # Sync mirrors to check for updates
 sudo pacman -Sy >/dev/null
 
-packagesCount="$(paru -Qu | wc -l)"
+packagesCount="$(paru -Qu 2>/dev/null | wc -l)"
 
 if [[ packagesCount -gt 0 ]]; then
   printf " $packagesCount"
