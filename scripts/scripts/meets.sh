4429fe952d0503e889f51789ea5fb245fe6cdcbd
Luis Simas <luissimas@protonmail.com>
2021-06-25 09:34:44 -0300

    Updating meets links

--- scripts/scripts/meets.sh
+++ scripts/scripts/meets.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-selected=$(echo -e "Probabilidade\nAtendimento Arq\nAtendimento LM\nAtendimento AED" | rofi -dmenu -i -p "Class: ")
+selected=$(echo -e "Probabilidade\nAtendimento LM\nIntroduction to Functional Programming" | rofi -dmenu -i -p "Class: ")
 
 case $selected in "Probabilidade")
   brave https://meet.google.com/dgv-bvnd-rjs?authuser=1
   ;;
-"Atendimento Arq")
-  brave https://meet.google.com/dfh-aotz-pfc?authuser=1
-  ;;
 "Atendimento LM")
   brave https://meet.google.com/yok-roat-gqg?authuser=1
   ;;
-"Atendimento AED")
-  brave https://meet.google.com/stt-xswo-tcz?authuser=1
+"Introduction to Functional Programming")
+  brave https://meet.google.com/vcz-tcrs-stb?authuser=0
   ;;
 esac
