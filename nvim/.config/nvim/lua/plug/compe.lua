61479670702b802f760dac1c7e3beed020076116
Luis Simas <luissimas@protonmail.com>
2021-06-27 10:42:23 -0300

    Fixing compe bindings

--- nvim/.config/nvim/lua/plug/compe.lua
+++ nvim/.config/nvim/lua/plug/compe.lua
@@ -20,7 +20,7 @@ require("compe").setup {
     path = true,
     spell = false,
     buffer = false,
-    tags = true,
+    tags = false,
     calc = true,
     omni = false,
     emoji = false,
@@ -36,32 +36,6 @@ require("compe").setup {
 -- Enabling lspkind icons
 require("lspkind").init()
 
---TODO: UltiSnips integration
-
--- Vsnip integration
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-n>"
-  else
-    return t "<Tab>"
-  end
-end
-
-_G.s_tab_complete = function()
-  if vim.fn.pumvisible() == 1 then
-    return t "<C-p>"
-  else
-    return t "<S-Tab>"
-  end
-end
-
 -- Completion toggle
 _G.compe_popup_toggle = function()
   if vim.fn.pumvisible() == 1 then
@@ -71,11 +45,11 @@ _G.compe_popup_toggle = function()
   end
 end
 
--- Tab keymaps
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+-- Next/prev completion items keymaps
+vim.api.nvim_set_keymap("i", "<C-j>", "<C-n>", {noremap = true, expr = false})
+vim.api.nvim_set_keymap("s", "<C-j>", "<C-n>", {noremap = true, expr = false})
+vim.api.nvim_set_keymap("i", "<C-k>", "<C-p>", {noremap = true, expr = false})
+vim.api.nvim_set_keymap("s", "<C-k>", "<C-p>", {noremap = true, expr = false})
 
 -- Manual completion popup toggle
 vim.api.nvim_set_keymap("i", "<C-Space>", "v:lua.compe_popup_toggle()", {noremap = true, expr = true, silent = true})
