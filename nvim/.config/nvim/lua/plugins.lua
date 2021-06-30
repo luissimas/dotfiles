c300649f88777a373c725d84fb96f9a1c12c2067
Luis Simas <luissimas@protonmail.com>
2021-06-30 19:56:06 -0300

    Refactoring plugin file

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -19,46 +19,218 @@ return require("packer").startup(
     use "nvim-lua/plenary.nvim" -- Utility functions for various plugins
 
     -- Editor
-    use "neovim/nvim-lspconfig" -- Lsp Config layer
-    use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"} -- Treesitter
-    use "mhartington/formatter.nvim" -- Code format - TODO: Check Neoformat or EFM
-    use "SirVer/ultisnips" -- Snippets engine
-    use "ray-x/lsp_signature.nvim" -- Function signature docs
-    use "honza/vim-snippets" -- Snippets collection
-    use "kyazdani42/nvim-tree.lua" -- File tree
-    use "hoob3rt/lualine.nvim" -- Better status line
-    use "folke/which-key.nvim" -- Never forget your keymaps
-    use "steelsojka/pears.nvim" -- I like autopairs
-    use "folke/trouble.nvim" -- Amazing quickfix lists
-    use "tpope/vim-commentary" -- Comment lines
-    use "tpope/vim-surround" -- Surround
-    -- use "ahmedkhalf/lsp-rooter.nvim" -- Change cwd based on the project's root directory
+    -- Lsp Config layer
+    use {
+      "neovim/nvim-lspconfig",
+      config = function()
+        require("plug.lspconfig")
+      end
+    }
+
+    -- Treesitter
+    use {
+      "nvim-treesitter/nvim-treesitter",
+      run = ":TSUpdate",
+      config = function()
+        require("plug.treesitter")
+      end
+    }
+
+    -- Code format - TODO: Check Neoformat or EFM
+    use {
+      "mhartington/formatter.nvim",
+      config = function()
+        require("plug.formatter")
+      end
+    }
+
+    -- Snippets engine
+    use {
+      "SirVer/ultisnips",
+      config = function()
+        require("plug.ultisnips")
+      end
+    }
+
+    -- Snippets collection
+    use "honza/vim-snippets"
+
+    -- Function signature docs
+    use {
+      "ray-x/lsp_signature.nvim",
+      config = function()
+        require("plug.lsp_signature")
+      end
+    }
+
+    -- File tree
+    use {
+      "kyazdani42/nvim-tree.lua",
+      config = function()
+        require("plug.tree")
+      end
+    }
+
+    -- Better status line
+    use {
+      "hoob3rt/lualine.nvim",
+      config = function()
+        require("plug.lualine")
+      end
+    }
+
+    -- Never forget your keymaps
+    use {
+      "folke/which-key.nvim",
+      config = function()
+        require("plug.whichkey")
+      end
+    }
+
+    -- I like autopairs
+    use {
+      "steelsojka/pears.nvim",
+      config = function()
+        require("plug.pears")
+      end
+    }
+
+    -- Better quickfix lists
+    use {
+      "folke/trouble.nvim",
+      config = function()
+        require("plug.trouble")
+      end
+    }
+
+    -- Comment lines
+    use "tpope/vim-commentary"
+
+    -- Surround
+    use "tpope/vim-surround"
+
+    -- Change cwd based on the project's root directory
+    -- use {
+    --   "ahmedkhalf/lsp-rooter.nvim",
+    --   config = function()
+    --     require("plug.lsp_rooter")
+    --   end
+    -- }
 
     -- Completion
-    use "hrsh7th/nvim-compe" -- Completion backend
-    use "ray-x/lsp_signature.nvim" -- Function signature docs
-    use "nvim-telescope/telescope.nvim" -- The ultimate fuzzy finder
-    use "nvim-telescope/telescope-fzy-native.nvim" -- Fzy extension for telescope
+    -- Completion backend
+    use {
+      "hrsh7th/nvim-compe",
+      config = function()
+        require("plug.compe")
+      end
+    }
+
+    -- The ultimate fuzzy finder
+    use {
+      "nvim-telescope/telescope.nvim",
+      config = function()
+        require("plug.telescope")
+      end
+    }
+
+    -- Fzy extension for telescope
+    use {"nvim-telescope/telescope-fzy-native.nvim"}
 
     -- UI
-    use "glepnir/lspsaga.nvim" -- Better lsp UI
-    use "kyazdani42/nvim-web-devicons" -- General icons
-    use "onsails/lspkind-nvim" -- Completion icons
-    use "norcalli/nvim-colorizer.lua" -- Colors in hex color codes
-    use "p00f/nvim-ts-rainbow" -- Color brackets
-    use "RRethy/vim-illuminate" -- Highlight words under cursor
-    use "folke/todo-comments.nvim" -- Highlight comments
+    -- Better lsp UI
+    use {
+      "glepnir/lspsaga.nvim",
+      config = function()
+        require("plug.lspsaga")
+      end
+    }
+
+    -- General icons
+    use {
+      "kyazdani42/nvim-web-devicons",
+      config = function()
+        require("plug.webdevicons")
+      end
+    }
+
+    -- Completion icons
+    use {
+      "onsails/lspkind-nvim",
+      config = function()
+        require("plug.lspkind")
+      end
+    }
+
+    -- Colors in hex color codes
+    use {
+      "norcalli/nvim-colorizer.lua",
+      config = function()
+        require("plug.colorizer")
+      end
+    }
+
+    -- Color brackets
+    use {"p00f/nvim-ts-rainbow"}
+
+    -- Highlight words under cursor
+    use {
+      "RRethy/vim-illuminate",
+      config = function()
+        require("plug.illuminate")
+      end
+    }
+
+    -- Highlight comments
+    use {
+      "folke/todo-comments.nvim",
+      config = function()
+        require("plug.todocomments")
+      end
+    }
 
     -- Tools
-    use "tpope/vim-fugitive" -- The best git integration plugin
-    use "TimUntersberger/neogit" -- The *other* best git integration plugin
-    use "lewis6991/gitsigns.nvim"
-    use "folke/lua-dev.nvim" -- Docs and completion for nvim lua API
-    use "milisims/nvim-luaref" -- Lua 5.1 reference manual
-    use {"lervag/vimtex", opt = true, ft = "tex"} -- Vimtex for latex
-    use "dstein64/vim-startuptime" -- Startup time log
-    use "aserowy/tmux.nvim" -- Navigate and resize tmux and vim splits
-    use "rmagatti/auto-session" -- Save and restore sessions
+    -- The best git integration plugin
+    use "tpope/vim-fugitive"
+
+    -- The *other* best git integration plugin
+    use {
+      "TimUntersberger/neogit",
+      config = function()
+        require("plug.neogit")
+      end
+    }
+
+    -- Git blame and signs
+    use {
+      "lewis6991/gitsigns.nvim",
+      config = function()
+        require("plug.gitsigns")
+      end
+    }
+
+    -- Docs and completion for nvim lua API
+    use "folke/lua-dev.nvim"
+
+    -- Lua 5.1 reference manual
+    use "milisims/nvim-luaref"
+
+    -- Vimtex for latex
+    use {"lervag/vimtex", opt = true, ft = "tex"}
+
+    -- Startup time log
+    use "dstein64/vim-startuptime"
+
+    -- Navigate and resize tmux and vim splits
+    use {
+      "aserowy/tmux.nvim",
+      config = function()
+        require("plug.tmux")
+      end
+    }
+
+    -- Save and restore sessions
+    use "rmagatti/auto-session"
 
     -- Colorschemes
     -- use "folke/lsp-colors.nvim" -- Adds LSP colors for themes that don't yet support them
