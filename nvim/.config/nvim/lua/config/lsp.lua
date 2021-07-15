b157f563034c27a292b474ad0467ab176ca74b16
Luis Simas <luissimas@protonmail.com>
2021-07-15 19:29:37 -0300

    Adding improvements from lspconfig ui

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,10 +1,7 @@
-local util = require("vim.lsp.util")
-
--- Updating diagnostics. See: https://github.com/folke/dot/blob/master/config/nvim/lua/config/lsp/diagnostics.lua
-vim.lsp.handlers["textDocument/publishDiagnostics"] =
-  vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics,
-  {
+-- Updating diagnostics. See: https://github.com/neovim/nvim-lspconfig/wiki/UI-customization
+vim.lsp.handlers["textDocument/publishDiagnostics"] = function(_, _, params, client_id, _)
+  local config = {
+    -- your config
     underline = true,
     update_in_insert = true,
     signs = true,
@@ -12,8 +9,29 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
     virtual_text = false,
     severity_sort = true
   }
-)
+  local uri = params.uri
+  local bufnr = vim.uri_to_bufnr(uri)
 
+  if not bufnr then
+    return
+  end
+
+  local diagnostics = params.diagnostics
+
+  for i, v in ipairs(diagnostics) do
+    diagnostics[i].message = string.format("%s: %s", v.source, v.message)
+  end
+
+  vim.lsp.diagnostic.save(diagnostics, bufnr, client_id)
+
+  if not vim.api.nvim_buf_is_loaded(bufnr) then
+    return
+  end
+
+  vim.lsp.diagnostic.display(diagnostics, bufnr, client_id, config)
+end
+
+-- Overriding default signs
 local signs = {Error = " ", Warning = " ", Hint = " ", Information = " "}
 
 for type, icon in pairs(signs) do
@@ -21,13 +39,14 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
 end
 
--- vim.lsp.handlers["textDocument/hover"] =
---   vim.lsp.with(
---   vim.lsp.handlers.hover,
---   {
---     border = "single"
---   }
--- )
+-- Adding borders for signature help and hover popup
+vim.cmd [[autocmd ColorScheme * highlight NormalFloat guibg=#1f2335]]
+vim.cmd [[autocmd ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
+
+local border = "solid"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {border = border})
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.hover, {border = border})
 
 -- Show diagnostics on cursor hold
-vim.cmd("autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()")
+vim.cmd("autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics({border = '" .. border .. "'})")
