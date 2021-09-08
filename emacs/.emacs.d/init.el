6254f25ab74613e7906e646922308545320f6603
Luis Simas <luissimas@protonmail.com>
2021-09-08 07:43:15 -0300

    Adding evil-collection

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -54,8 +54,16 @@
 
 ;; Evil-mode
 (use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
   :config
   (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
 
 ;; Magit
 (use-package magit)
@@ -63,19 +71,3 @@
 ;; Doom modeline
 (use-package doom-modeline
   :init (doom-modeline-mode 1))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("4b0e826f58b39e2ce2829fab8ca999bcdc076dec35187bf4e9a4b938cb5771dc" "7eea50883f10e5c6ad6f81e153c640b3a288cd8dc1d26e4696f7d40f754cc703" "5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" "234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" "47db50ff66e35d3a440485357fb6acb767c100e135ccdf459060407f8baea7b2" default))
- '(package-selected-packages
-   '(magit doom-modeline evil evil-mode doom-themes use-package ivy)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
