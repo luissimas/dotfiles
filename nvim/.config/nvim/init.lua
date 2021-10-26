9ff8c273a40128f1a8d2af82eafed485edb8b443
Luis Simas <luissimas@protonmail.com>
2021-10-26 19:16:00 -0300

    Organizing and null-ls setup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,105 +1,6 @@
--- Tabs
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
-vim.o.expandtab = true
-vim.o.smartindent = true
-
--- Number column
-vim.o.relativenumber = true
-vim.o.number = true
-vim.o.numberwidth = 1
-
--- Search
-vim.o.hlsearch = false
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.o.incsearch = true
-
--- Recover files
-vim.o.swapfile = false
-vim.o.undofile = true
-
--- Split orientation
-vim.o.splitbelow = true
-vim.o.splitright = true
-
--- Misc
-vim.o.termguicolors = true -- enable true color support
-vim.o.wrap = false -- no line wrap
-vim.o.foldenable = false -- disable folding
-vim.o.hidden = true -- buffers can be kept open in the background
-vim.o.inccommand = "nosplit" -- visual incremental feedback for commands
-vim.o.scrolloff = 8 -- scroll offset
-vim.o.showmode = false -- don't show current mode in command area
-vim.o.showtabline = 0 -- never show tablines
-vim.o.signcolumn = "yes" -- set signcolumn display
-vim.o.completeopt = "menuone,noinsert" -- completion options
--- vim.o.colorcolumn = "80" -- column for visual indent guideline
-vim.o.updatetime = 200 -- time for CursorHold event
-vim.o.clipboard = "unnamedplus" -- setting clipboard to system's
-vim.opt.shortmess = vim.opt.shortmess + "c" -- disable completion item messages
-vim.g.mapleader = " " -- leader key
-
--- Statusline
-function _G.gitbranch()
-  local branch = vim.fn.system(
-    "git -C "
-    .. vim.fn.expand('%:h') ..
-    " branch --show-current 2>/dev/null"
-  ):gsub("\n", "")
-
-  if branch ~= "" then
-    return string.format("[%s] ", branch)
-  end
-
-  return ""
-end
-
--- vim.o.statusline = " %{v:lua.gitbranch()}%f %m %r %= %y %p%% "
-vim.o.statusline = " %f %m %r %= %y %p%% "
-
--- Quickfixlist
-vim.api.nvim_exec(
-  [[
-  augroup QuickFixSettings
-    autocmd!
-    autocmd BufWrite,BufEnter,InsertLeave * lua vim.diagnostic.setloclist({ open = false })
-    autocmd FileType qf set nonumber
-    autocmd FileType qf set norelativenumber
-    autocmd FileType qf set signcolumn="no"
-    autocmd FileType qf map <buffer> q :cclose<Enter>
-  augroup end
-]],
-    false
-)
-
-vim.api.nvim_set_keymap("n", "<C-q>", ":copen<Enter>", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("n", "<C-n>", ":cnext<Enter>", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("n", "<C-p>", ":cprev<Enter>", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("n", "<leader>l", ":lopen<Enter>", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("n", "<leader>n", ":lnext<Enter>", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("n", "<leader>p", ":lprev<Enter>", { noremap=true, silent=true })
-
--- Netrw
-vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 15
-vim.g.netrw_browse_split = 4
-vim.g.netrw_hide = 0
-vim.g.netrw_liststyle = 3
-vim.g.netrw_list_hide = [[node_modules,\.git]]
-
-vim.api.nvim_exec(
-  [[
-  augroup NetrwSettings
-    autocmd!
-    autocmd FileType netrw setlocal bufhidden=wipe
-  augroup end
-]],
-  false
-)
-
-vim.api.nvim_set_keymap("n", "<leader>e", ":Lexplore<Enter>", { noremap=true, silent=true })
+-- Leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Trim whitespace on save
 vim.api.nvim_exec(
@@ -130,10 +31,10 @@ vim.api.nvim_exec(
 )
 
 -- Packer bootstrap
-local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  Bootstraped = vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  Bootstraped = vim.fn.system { "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path }
 end
 
 -- Compiling packer after saving file
@@ -148,46 +49,57 @@ vim.api.nvim_exec(
 )
 
 -- Plugins
-require("packer").startup({function(use)
-  use "wbthomason/packer.nvim" -- Package manager
-  use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" } -- Treesitter
-  use "neovim/nvim-lspconfig" -- LSP client configuration
-  use "hrsh7th/nvim-cmp" -- Autocompletion
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-nvim-lua"
-  use "hrsh7th/cmp-path"
-  use "SirVer/UltiSnips" -- Snippets engine
-  use "honza/vim-snippets" -- Snippets collection
-  use { "nvim-telescope/telescope.nvim", requires = { "nvim-lua/plenary.nvim" } } -- Fuzzy finder
-  use "aserowy/tmux.nvim" -- Tmux navigation
-  use "RRethy/nvim-base16" -- Colorschemes
+require("packer").startup {
+  function(use)
+    use "wbthomason/packer.nvim"
+    use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
+    use "neovim/nvim-lspconfig"
+    use "nvim-lua/plenary.nvim"
+    use "jose-elias-alvarez/null-ls.nvim"
+    use "hrsh7th/nvim-cmp"
+    use "hrsh7th/cmp-path"
+    use "hrsh7th/cmp-nvim-lsp"
+    use "hrsh7th/cmp-nvim-lua"
+    use "kdheepak/cmp-latex-symbols"
+    use "quangnguyen30192/cmp-nvim-ultisnips"
+    use "SirVer/UltiSnips"
+    use "honza/vim-snippets"
+    use "nvim-telescope/telescope.nvim"
+    use "aserowy/tmux.nvim"
+    use "RRethy/nvim-base16"
+    use "onsails/lspkind-nvim"
+    use "lewis6991/gitsigns.nvim"
 
-  -- Automatically set up packer after cloning it
-  if Bootstraped then
-    require('packer').sync()
-  end
-end,
-config = {
-  display = {
-    open_fn = function()
-      return require("packer.util").float({ border = "single" })
+    -- Automatically set up packer after cloning it
+    if Bootstraped then
+      require("packer").sync()
     end
-  }
-}})
+  end,
+  config = {
+    display = {
+      open_fn = function()
+        return require("packer.util").float { border = "single" }
+      end,
+    },
+  },
+}
 
 -- Save and quit
-vim.api.nvim_set_keymap("n", "<leader>w", ":w<Enter>", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("n", "<leader>q", ":q<Enter>", { noremap=true, silent=true })
+vim.api.nvim_set_keymap("n", "<leader>w", ":w<Enter>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>q", ":q<Enter>", { noremap = true, silent = true })
+
+-- Kill buffers without messing up the split
+vim.api.nvim_set_keymap("n", "<leader>bk", ":bp <bar> vs <bar> bn <bar> bd<Enter>", { noremap = true, silent = true })
 
 -- Splits
 vim.api.nvim_set_keymap("n", "<C-s>", ":vsplit<Enter>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-x>", ":split<Enter>", { noremap = true, silent = true })
 
 -- Movement with line wraps
-vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
-vim.api.nvim_set_keymap('v', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
-vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
-vim.api.nvim_set_keymap('v', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap("v", "k", "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap("n", "j", "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap("v", "j", "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
 
 -- Move to begin/end of line with L and H
 vim.api.nvim_set_keymap("n", "<S-l>", "$", { noremap = true, silent = true })
@@ -196,7 +108,7 @@ vim.api.nvim_set_keymap("v", "<S-l>", "$", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<S-h>", "0", { noremap = true, silent = true })
 
 -- Tmux navigation bindings
-require("tmux").setup({
+require("tmux").setup {
   navigation = {
     -- cycles to opposite pane while navigating into the border
     cycle_navigation = false,
@@ -213,7 +125,7 @@ require("tmux").setup({
     -- sets resize steps for y axis
     resize_step_y = 5,
   },
-})
+}
 
 vim.cmd "colorscheme base16-gruvbox-dark-hard"
 
@@ -222,96 +134,16 @@ vim.cmd "hi VertSplit guifg=bg"
 vim.cmd "hi NonText guifg=bg"
 
 -- Treesitter
-require("nvim-treesitter.configs").setup({
+require("nvim-treesitter.configs").setup {
   ensure_installed = "all",
 
   highlight = {
     enable = true,
   },
   indent = {
-    enable = true
-  }
-})
-
--- LSP settings
-local nvim_lsp = require("lspconfig")
-local on_attach = function(_, bufnr)
-  -- Setting lsp completion with omnifunc
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-  local map = vim.api.nvim_buf_set_keymap
-  local opts = { noremap = true, silent = true }
-
-  map(bufnr, "n", "gd", ":lua vim.lsp.buf.definition()<Enter>", opts)
-  map(bufnr, "n", "K", ":lua vim.lsp.buf.hover()<Enter>", opts)
-  map(bufnr, "n", "gi", ":lua vim.lsp.buf.implementation()<Enter>", opts)
-  map(bufnr, "n", "gr", ":lua vim.lsp.buf.references()<Enter>", opts)
-  map(bufnr, "n", "<leader>d", ":lua vim.lsp.diagnostic.show_line_diagnostics()<Enter>", opts)
-  map(bufnr, "n", "<leader>rn", ":lua vim.lsp.buf.rename()<Enter>", opts)
-  map(bufnr, "n", "<leader>ca", ":lua vim.lsp.buf.code_action()<Enter>", opts)
-end
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
-
-local servers = {
-  "clangd", "tsserver", "bashls", "vimls", "jsonls", "html", "cssls", "ocamllsp"
-}
-
-for _, server in ipairs(servers) do
-  nvim_lsp[server].setup({
-    on_attach = on_attach,
-    capabilities = capabilities
-  })
-end
-
-local sumneko_binary = "/usr/bin/lua-language-server"
-
-nvim_lsp.sumneko_lua.setup {
-  cmd = { sumneko_binary },
-  on_attach = on_attach,
-  capabilities = capabilities,
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-        -- Setup your lua path
-        path = vim.split(package.path, ";")
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file('', true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
+    enable = true,
   },
 }
-
-nvim_lsp.elixirls.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  cmd = { vim.fn.expand("~/repos/elixir-ls/language_server.sh") },
-  settings = {
-    elixirLS = {
-      dialyzerEnabled = false,
-    },
-  },
-}
-
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  virtual_text = true,
-  signs = true,
-  underline = true,
-  update_in_insert = true,
-})
 
 -- UltiSnips
 vim.g.UltiSniptsEditSplit = "normal"
@@ -321,9 +153,9 @@ vim.g.UltiSnipsJumpForwardTrigger = "<Tab>"
 vim.g.UltiSnipsJumpBackwardTrigger = "<S-tab>"
 
 -- Cmp
-local cmp = require("cmp")
+local cmp = require "cmp"
 
-cmp.setup({
+cmp.setup {
   completion = {
     keyword_length = 3,
   },
@@ -333,60 +165,82 @@ cmp.setup({
     end,
   },
   mapping = {
-      ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-d>"] = cmp.mapping.scroll_docs(4),
-      ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-      ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.close(),
-      ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+    ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+    ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+  },
+  sources = {
+    { name = "nvim_lua" },
+    { name = "nvim_lsp" },
+    { name = "path" },
+    { name = "latex_symbols" },
+    { name = "ultisnips" },
+  },
+  documentation = {
+    border = "single",
+  },
+  formatting = {
+    format = require("lspkind").cmp_format {
+      with_text = false,
+      menu = {
+        nvim_lua = "[Lua]",
+        nvim_lsp = "[LSP]",
+        path = "[Path]",
+        latex_symbols = "[Latex]",
+        ultisnips = "[UltiSnips]",
+      },
     },
-    sources = {
-      { name = "nvim_lua" },
-      { name = "nvim_lsp" },
-      { name = "path" },
-      { name = "ultisnips" },
-    },
-    experimental = {
-      native_menu = false,
-      ghost_text = true,
-    },
-})
+  },
+  experimental = {
+    native_menu = false,
+    ghost_text = true,
+  },
+}
 
 -- Telescope
 require("telescope").setup {
   defaults = {
     prompt_prefix = " ",
-        selection_caret = " ",
-        entry_prefix = " ",
-        sorting_strategy = "descending",
-        layout_strategy = "horizontal",
-        layout_config = {
-          horizontal = {
-            preview_width = 0.6,
-          },
-        },
-        mappings = {
-          i = {
-            ["<C-s>"] = "file_vsplit",
-            ["<C-x>"] = "file_split",
-            ["<C-k>"] = "move_selection_previous",
-            ["<C-j>"] = "move_selection_next",
-          },
-          n = {
-            ["<C-s>"] = "file_vsplit",
-            ["<C-x>"] = "file_split",
-            ["<leader>q"] = "close",
-          }
-        },
-        file_ignore_patterns = { "node_modules", "_build", ".elixir_ls", "%.png", "%.jpg", "%.jpeg", "%.pdf" },
-      }
+    selection_caret = " ",
+    entry_prefix = " ",
+    sorting_strategy = "descending",
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        preview_width = 0.6,
+      },
+    },
+    mappings = {
+      i = {
+        ["<C-s>"] = "file_vsplit",
+        ["<C-x>"] = "file_split",
+        ["<C-k>"] = "move_selection_previous",
+        ["<C-j>"] = "move_selection_next",
+      },
+      n = {
+        ["<C-s>"] = "file_vsplit",
+        ["<C-x>"] = "file_split",
+        ["<leader>q"] = "close",
+      },
+    },
+    file_ignore_patterns = { "node_modules", "_build", ".elixir_ls", "%.png", "%.jpg", "%.jpeg", "%.pdf" },
+  },
 }
 
-vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<Enter>", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope live_grep<Enter>", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("n", "<leader>fs", ":Telescope grep_string<Enter>", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope buffers<Enter>", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope help_tags<Enter>", { noremap=true, silent=true })
-vim.api.nvim_set_keymap("n", "<leader>fm", ":Telescope man_pages<Enter>", { noremap=true, silent=true })
+vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<Enter>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope live_grep<Enter>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fw", ":Telescope grep_string<Enter>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fb", ":Telescope buffers<Enter>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope help_tags<Enter>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fm", ":Telescope man_pages<Enter>", { noremap = true, silent = true })
 
+-- Gitsigns
+require("gitsigns").setup {
+  keymaps = {
+    ["n <leader>gb"] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
+  },
+}
---
+++ nvim/.config/nvim/plugin/lsp.lua
@@ -0,0 +1,130 @@
+-- LSP settings
+local nvim_lsp = require "lspconfig"
+local border = "single"
+
+local diagnostics_opts = {
+  virtual_text = { spacing = 4, prefix = "●" },
+  signs = true,
+  underline = true,
+  update_in_insert = true,
+  source = "if_many",
+}
+
+-- Signs
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- Handlers
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border })
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border })
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, diagnostics_opts)
+
+-- Null-ls
+local null_ls = require "null-ls"
+
+null_ls.config {
+  sources = {
+    null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.formatting.json_tool,
+    null_ls.builtins.formatting.mix,
+    null_ls.builtins.formatting.sqlformat,
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.diagnostics.credo,
+    null_ls.builtins.diagnostics.eslint_d,
+    null_ls.builtins.diagnostics.shellcheck,
+    null_ls.builtins.diagnostics.write_good,
+    null_ls.builtins.diagnostics.vint,
+  },
+}
+
+local on_attach = function(client, bufnr)
+  -- Setting lsp completion with omnifunc
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+  -- Mappings
+  local map = vim.api.nvim_buf_set_keymap
+  local opts = { noremap = true, silent = true }
+
+  map(bufnr, "n", "gd", ":lua vim.lsp.buf.definition()<Enter>", opts)
+  map(bufnr, "n", "K", ":lua vim.lsp.buf.hover()<Enter>", opts)
+  map(bufnr, "n", "gi", ":lua vim.lsp.buf.implementation()<Enter>", opts)
+  map(bufnr, "n", "gr", ":lua vim.lsp.buf.references()<Enter>", opts)
+  map(bufnr, "n", "<leader>d", ":lua vim.lsp.diagnostic.show_line_diagnostics()<Enter>", opts)
+  map(bufnr, "n", "<leader>rn", ":lua vim.lsp.buf.rename()<Enter>", opts)
+  map(bufnr, "n", "<leader>ca", ":lua vim.lsp.buf.code_action()<Enter>", opts)
+
+  -- Enabling only null-ls formatting
+  if client.name == "null-ls" and client.resolved_capabilities.document_formatting then
+    vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+  else
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+  end
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+
+local servers = {
+  "null-ls",
+  "clangd",
+  "tsserver",
+  "bashls",
+  "vimls",
+  "jsonls",
+  "html",
+  "cssls",
+  "ocamllsp",
+}
+
+for _, server in ipairs(servers) do
+  nvim_lsp[server].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+local sumneko_binary = "/usr/bin/lua-language-server"
+
+nvim_lsp.sumneko_lua.setup {
+  cmd = { sumneko_binary },
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+        -- Setup your lua path
+        path = vim.split(package.path, ";"),
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { "vim" },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
+nvim_lsp.elixirls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  cmd = { vim.fn.expand "~/repos/elixir-ls/language_server.sh" },
+  settings = {
+    elixirLS = {
+      dialyzerEnabled = false,
+    },
+  },
+}
