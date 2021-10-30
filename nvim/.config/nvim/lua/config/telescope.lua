852922a842f678cbc924f5cc956bcd664cb37992
Luis Simas <luissimas@protonmail.com>
2021-10-30 10:52:42 -0300

    Telescope setup

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -1,10 +1,31 @@
-require("telescope").setup {
+local telescope = require "telescope"
+local previewers = require "telescope.previewers"
+local preview_utils = require "telescope.previewers.utils"
+local filetype = require "plenary.filetype"
+
+local custom_preview = function(filepath, bufnr, opts)
+  opts = opts or {}
+
+  if opts.use_ft_detect == nil then
+    local ft = filetype.detect(filepath)
+
+    if ft == "elixir" then
+      opts.use_ft_detect = false
+      preview_utils.regex_highlighter(bufnr, ft)
+    end
+  end
+
+  previewers.buffer_previewer_maker(filepath, bufnr, opts)
+end
+
+telescope.setup {
   defaults = {
     prompt_prefix = " ",
     selection_caret = " ",
     entry_prefix = " ",
     sorting_strategy = "descending",
     layout_strategy = "horizontal",
+    buffer_previewer_maker = custom_preview,
     layout_config = {
       horizontal = {
         preview_width = 0.6,
@@ -27,13 +48,35 @@ require("telescope").setup {
   },
 }
 
-require('telescope').load_extension('fzf')
-require("telescope").load_extension "projects"
+telescope.load_extension "fzf"
+telescope.load_extension "projects"
 
-vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<Enter>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fg", ":Telescope live_grep<Enter>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fw", ":Telescope grep_string<Enter>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>bb", ":Telescope buffers<Enter>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fh", ":Telescope help_tags<Enter>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fm", ":Telescope man_pages<Enter>", { noremap = true, silent = true })
+local function builtin(picker)
+  return string.format(":lua require('telescope.builtin').%s() <Enter>", picker)
+end
+
+local function config(picker)
+  return string.format(":lua require('config.telescope').%s() <Enter>", picker)
+end
+
+vim.api.nvim_set_keymap("n", "<leader>ff", config "find_files", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fg", builtin "live_grep", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fw", builtin "grep_string", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fb", builtin "current_buffer_fuzzy_find", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fh", builtin "help_tags", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>fm", builtin "man_pages", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>bb", builtin "buffers", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>fp", ":Telescope projects<Enter>", { noremap = true, silent = true })
+
+-- Custom pickers
+local M = {}
+
+M.find_files = function()
+  local ok = pcall(require("telescope.builtin").git_files)
+
+  if not ok then
+    require("telescope.builtin").find_files()
+  end
+end
+
+return M
