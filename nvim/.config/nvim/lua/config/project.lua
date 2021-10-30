38b30350f3368a60dcc4178f4c859fce003da107
Luis Simas <luissimas@protonmail.com>
2021-10-30 11:50:49 -0300

    Project root patterns

--- nvim/.config/nvim/lua/config/project.lua
+++ nvim/.config/nvim/lua/config/project.lua
@@ -1,4 +1,5 @@
 require("project_nvim").setup {
+  patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "mix.exs", "dune" },
   ignore_lsp = { "null-ls" },
   show_hidden = true,
 }
