8e9bcd9bfea787125abbe382ab46aadd4049f500
Luis Simas <luissimas@protonmail.com>
2022-08-14 13:15:40 -0300

    back to bspwm

--- polybar/.config/polybar/scripts/temp.sh
+++ polybar/.config/polybar/scripts/temp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ $(find /sys/class/power_supply -name "*BAT*") ]; then
+	# Laptop
+	temp="$(sensors 2>/dev/null | grep 'Core 0' | awk 'NR==1{printf "%d", $3}')"
+else
 	# Desktop
 	temp="$(sensors 2>/dev/null | grep Tctl | awk 'NR==1{printf "%d", $2}')"
-else
-	# Laptop
-	temp="$(sensors 2>/dev/null | grep temp1 | awk 'NR==1{printf "%d", $2}')"
 fi
 
 printf " $temp°C"
