8b5bb138bc15322c7171e806f9dea377d660fabc
Luís Simas <luissimas@protonmail.com>
2023-01-20 10:41:36 -0300

    fixing books script

--- scripts/scripts/list-books.sh
+++ scripts/scripts/list-books.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-booksDirectory=/home/padawan/repos/books
+booksDirectory=/home/padawan/repos/books/
 
-function get_books() {
-    ls $wallpaperDirectory
-}
-
-selected=$(ls -R $booksDirectory | grep '.pdf\|.epub\|.djvu' | sed 's/\/home\/padawan\/repos\/books\///' | rofi -dmenu -i -p "Select a file")
+selected=$(fd . $booksDirectory -t f | sed 's/\/home\/padawan\/repos\/books\///' | rofi -dmenu -i -p "Select a file")
 
 if [ -z "$selected" ]; then
     echo "No book selected"
 else
-    zathura $booksDirectory/$selected
+    zathura $booksDirectory/"$selected"
 fi
