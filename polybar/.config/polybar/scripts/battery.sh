446b542d1926ddeb8b1042c88bde76a2e876eea2
Luís Simas <luissimas@protonmail.com>
2023-01-21 18:02:51 -0300

    polybar merge batteries data

--- polybar/.config/polybar/scripts/battery.sh
+++ polybar/.config/polybar/scripts/battery.sh
@@ -1,20 +1,82 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ -e "/sys/class/power_supply/BAT0" ]; then
-	batteryStatus="$(cat /sys/class/power_supply/BAT*/status)"
-	capacity="$(cat /sys/class/power_supply/BAT*/capacity)"
+battery_print() {
+    PATH_AC="/sys/class/power_supply/AC"
+    PATH_BATTERY_0="/sys/class/power_supply/BAT0"
+    PATH_BATTERY_1="/sys/class/power_supply/BAT1"
 
-	if [ $batteryStatus = "Charging" ]; then
-		icon=""
-	elif [ $capacity -ge 75 ]; then
-		icon=" "
-	elif [ $capacity -lt 75 ] && [ $capacity -ge 50 ]; then
-		icon=" "
-	elif [ $capacity -lt 50 ] && [ $capacity -ge 25 ]; then
-		icon=" "
-	elif [ $capacity -lt 25 ] && [ $capacity -ge 0 ]; then
-		icon=" "
-	fi
+    ac=0
+    battery_level_0=0
+    battery_level_1=0
+    battery_max_0=0
+    battery_max_1=0
 
-	printf "$icon $capacity%%"
-fi
+    if [ -f "$PATH_AC/online" ]; then
+        ac=$(cat "$PATH_AC/online")
+    fi
+
+    if [ -f "$PATH_BATTERY_0/energy_now" ]; then
+        battery_level_0=$(cat "$PATH_BATTERY_0/energy_now")
+    fi
+
+    if [ -f "$PATH_BATTERY_0/energy_full" ]; then
+        battery_max_0=$(cat "$PATH_BATTERY_0/energy_full")
+    fi
+
+    if [ -f "$PATH_BATTERY_1/energy_now" ]; then
+        battery_level_1=$(cat "$PATH_BATTERY_1/energy_now")
+    fi
+
+    if [ -f "$PATH_BATTERY_1/energy_full" ]; then
+        battery_max_1=$(cat "$PATH_BATTERY_1/energy_full")
+    fi
+
+    battery_level=$(("$battery_level_0 + $battery_level_1"))
+    battery_max=$(("$battery_max_0 + $battery_max_1"))
+
+    battery_percent=$(("$battery_level * 100"))
+    battery_percent=$(("$battery_percent / $battery_max"))
+
+    if [ "$ac" -eq 1 ]; then
+        icon="ﮣ"
+    else
+        if [ "$battery_percent" -gt 85 ]; then
+            icon=" "
+        elif [ "$battery_percent" -gt 60 ]; then
+            icon=" "
+        elif [ "$battery_percent" -gt 35 ]; then
+            icon=" "
+        elif [ "$battery_percent" -gt 10 ]; then
+            icon=" "
+        else
+            icon=" "
+        fi
+    fi
+
+    echo "$icon $battery_percent%"
+}
+
+path_pid="/tmp/polybar-battery-combined-udev.pid"
+
+case "$1" in
+--update)
+    pid=$(cat $path_pid)
+
+    if [ "$pid" != "" ]; then
+        kill -10 "$pid"
+    fi
+    ;;
+*)
+    echo $$ >$path_pid
+
+    trap exit INT
+    trap "echo" USR1
+
+    while true; do
+        battery_print
+
+        sleep 30 &
+        wait
+    done
+    ;;
+esac
