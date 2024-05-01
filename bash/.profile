e9d4c1696a4907ac1381f38e6ab125c9ce1f2925
Luís Simas <luissimas@protonmail.com>
2024-05-01 09:39:00 -0300

    chore: deleting window manager configuration

--- bash/.profile
+++
@@ -1,6 +0,0 @@
-# Source bashrc
-if [ -n "$BASH_VERSION" ]; then
-    if [ -f "$HOME"/.bashrc ]; then
-        source "$HOME"/.bashrc
-    fi
-fi
--- pywal/.config/wal/templates/colors-wal-dwm.h
+++
@@ -1,7 +0,0 @@
-static const char norm_fg[] = "{foreground}";
-static const char norm_bg[] = "{background}";
-static const char norm_border[] = "{background}";
-
-static const char sel_fg[] = "{background}";
-static const char sel_bg[] = "{foreground}";
-static const char sel_border[] = "{foreground}";
