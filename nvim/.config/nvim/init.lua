ff0621889d805e752e73d3d7a73a184e20286307
Luís Simas <luissimas@protonmail.com>
2024-05-08 19:39:09 -0300

    neovim: set tabstop to 4

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -16,6 +16,9 @@ vim.opt.relativenumber = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
+-- Set tab width to 4 spaces
+vim.opt.tabstop = 4
+
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
