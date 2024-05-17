5b76c2bb049a47198f223c9a7e7235f84560b0f8
Luís Simas <luissimas@protonmail.com>
2024-05-17 09:56:13 -0300

    neovim: json schemas

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -13,6 +13,9 @@ return {
     -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
     -- used for completion, annotations and signatures of Neovim apis
     { 'folke/neodev.nvim', opts = {} },
+
+    -- yamlls and jsonls schemas
+    'b0o/schemastore.nvim',
   },
   config = function()
     --  This function gets run when an LSP attaches to a particular buffer.
@@ -130,21 +133,11 @@ return {
       yamlls = {
         settings = {
           yaml = {
-            schemas = {
-              kubernetes = '*.yaml',
-              ['http://json.schemastore.org/github-workflow'] = '.github/workflows/*',
-              ['http://json.schemastore.org/github-action'] = '.github/action.{yml,yaml}',
-              ['http://json.schemastore.org/ansible-stable-2.9'] = 'roles/tasks/*.{yml,yaml}',
-              ['http://json.schemastore.org/prettierrc'] = '.prettierrc.{yml,yaml}',
-              ['http://json.schemastore.org/kustomization'] = 'kustomization.{yml,yaml}',
-              ['http://json.schemastore.org/ansible-playbook'] = '*play*.{yml,yaml}',
-              ['http://json.schemastore.org/chart'] = 'Chart.{yml,yaml}',
-              ['https://json.schemastore.org/dependabot-v2'] = '.github/dependabot.{yml,yaml}',
-              ['https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json'] = '*gitlab-ci*.{yml,yaml}',
-              ['https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json'] = '*api*.{yml,yaml}',
-              ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = '*docker-compose*.{yml,yaml}',
-              ['https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json'] = '*flow*.{yml,yaml}',
+            schemaStore = {
+              enable = false,
+              url = '',
             },
+            schemas = require('schemastore').yaml.schemas(),
           },
         },
       },
