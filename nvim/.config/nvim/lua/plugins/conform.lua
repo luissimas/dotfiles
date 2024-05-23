14584d14e18092d36f112c64e395cf5f800f2730
Luís Simas <luissimas@protonmail.com>
2024-05-23 07:41:46 -0300

    neovim: format markdown

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -27,6 +27,7 @@ return {
     formatters_by_ft = {
       lua = { 'stylua' },
       go = { 'goimports', 'gofmt' },
+      markdown = { 'mdformat' },
       -- Conform can also run multiple formatters sequentially
       -- python = { 'isort', 'black' },
       --
