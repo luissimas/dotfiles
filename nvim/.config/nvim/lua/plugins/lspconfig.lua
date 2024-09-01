69fd057c04385f36c79c7c59df2b082508d97cd0
Luís Simas <luissimas@protonmail.com>
2024-09-01 15:24:03 -0300

    neovim: typescript setup

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -128,6 +128,7 @@ return {
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       -- clangd = {},
+      tsserver = {},
       rust_analyzer = {},
       bashls = {},
       gopls = {
