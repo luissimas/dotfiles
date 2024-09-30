1160aec16b00a7f9bc1c23ebf857a6660244a1d4
Luís Simas <luissimas@protonmail.com>
2024-09-30 09:06:02 -0300

    neovim: catpuccin colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,14 @@
 -- Colorschemes
 return {
-  'ellisonleao/gruvbox.nvim',
+  'catppuccin/nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
-  opts = { contrast = 'hard' },
+  -- opts = { contrast = 'hard' },
   init = function()
     -- Load the colorscheme here.
-    vim.cmd.colorscheme 'gruvbox'
+    vim.cmd.colorscheme 'catppuccin-latte'
 
     -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-    vim.cmd.hi 'SignColumn guibg=none'
+    -- vim.cmd.hi 'Comment gui=none'
+    -- vim.cmd.hi 'SignColumn guibg=none'
   end,
 }
