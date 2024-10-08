254fad0e73defb24daf5ce7ec8505850f389c047
Luís Simas <luissimas@protonmail.com>
2024-10-08 08:06:22 -0300

    neovim: rename tsserver to ts_ls

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -128,7 +128,7 @@ return {
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       -- clangd = {},
-      tsserver = {},
+      ts_ls = {},
       rust_analyzer = {},
       bashls = {},
       gopls = {
