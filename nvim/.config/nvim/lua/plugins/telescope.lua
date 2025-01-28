788530bb67ae3a4e440d89221ce68656df8380af
Luís Simas <luissimas@protonmail.com>
2025-01-28 10:59:59 -0300

    neovim: smart path display on telescope
    Signed-off-by: Luís Simas <luissimas@protonmail.com>

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -25,6 +25,9 @@ return {
   },
   config = function()
     require('telescope').setup {
+      defaults = {
+        path_display = { 'smart' },
+      },
       pickers = {
         find_files = {
           find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*', '-L' },
