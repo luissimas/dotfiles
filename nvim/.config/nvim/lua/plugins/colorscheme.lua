826d996d79d72ab4344d58b947bfbb20eaf7dac0
Luís Simas <luissimas@protonmail.com>
2025-10-29 17:11:40 -0300

    nvim: colorscheme
    Signed-off-by: Luís Simas <luissimas@protonmail.com>

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,11 @@
 -- Colorschemes
 return {
-  'scottmckendry/cyberdream.nvim',
+  'metalelf0/black-metal-theme-neovim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
-  opts = {
-    variant = 'auto',
-    transparent = true,
-  },
-  init = function()
-    vim.cmd.colorscheme 'cyberdream'
+  config = function()
+    require('black-metal').setup {
+      transparent = true,
+    }
+    require('black-metal').load()
   end,
 }
